da15d5fdb98a404018202bd8d685e403bc2202c5
Bonnie Wall <bonniew@missionreadyhq.com>
2024-09-11 19:15:43 +1200

    my cats are cute gremlins

--- script.js
+++ script.js
@@ -8,7 +8,9 @@ const images = [
     { src: 'images/snickers2.jpg', name: 'Snickers 2' },
     { src: 'images/snickers3.jpg', name: 'Snickers 3' },
     { src: 'images/snickers4.jpg', name: 'Snickers 4' },
-    { src: 'images/snickers5.jpg', name: 'Snickers 5' }
+    { src: 'images/snickers5.jpg', name: 'Snickers 5' },
+    { src: 'images/Oreo.jpg', name: 'Oreo' },
+    { src: 'images/Tinky.png', name: 'Tinky' }
 ];
 
 let currentIndex = 0;
@@ -16,16 +18,19 @@ let currentIndex = 0;
 
 // get elements by id for selected-Image, image-name, left-arrow and right arrow
 // get elements by query selector all for .thumbnail
-const selectedImage = null
-const imageName = null
-const leftArrow = null
-const rightArrow = null
-const thumbnails = null
+const selectedImage = document.getElementById("selected-image")
+const imageName = document.getElementById("image-name")
+const leftArrow = document.getElementById("left-arrow")
+const rightArrow = document.getElementById("right-arrow")
+const thumbnails = document.querySelectorAll('.thumbnail')
 
 // to update the selected image and the displayed image name
 function updateGallery() {
-
+    selectedImage.src = images[currentIndex].src
+    imageName.innerHTML = images[currentIndex].name
 }
+
+updateGallery(); // sets it the first time???
 
 
 // to change current index to the next one
@@ -33,20 +38,36 @@ function updateGallery() {
 // call update gallery
 function nextImage() {
     
+    currentIndex++;
+   if (currentIndex === images.length) {
+    currentIndex = 0;
+   }
+   updateGallery()
+
 }
 
 // to change current index to the prevous one
 // if at begining of gallery, change to end
 // call update gallery
 function prevImage() {
-   
+   currentIndex--;
+   if (currentIndex < 0) {
+    currentIndex = images.length - 1;
+   }
+   updateGallery()
 }
 
 // change current index to selected thumbnail index
 // call updateGallery
 function selectThumbnail(index){
-   
+   currentIndex = index;
+   updateGallery()
 }
 
 // add functions to elements by adding event listners or directly in the html
+leftArrow.addEventListener('click', prevImage);
+rightArrow.addEventListener('click', nextImage);
 
+thumbnails.forEach((thumbnail, index) => {
+    thumbnail.addEventListener("click", () => selectThumbnail(index))
+})
